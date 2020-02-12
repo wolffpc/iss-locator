f51ec49b150822d254386f50b99b92a9ba90ce41
Peter Wolff <wolffpc1@gmail.com>
2020-02-12 12:58:25 -0800

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,41 @@
+const long = document.querySelector('#longitude')
+const lat = document.querySelector('#latitude')
+
+// Initialize map and marker
+const mymap = L.map('mapid').setView([0, 0], 2)
+const satelliteIcon = L.icon({
+    iconUrl: '../img/satellite.png',
+    iconSize: [30,30]
+})
+const marker = L.marker([0, 0], {icon: satelliteIcon}).addTo(mymap)
+
+// Setup map tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar', attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+}).addTo(mymap)
+
+// Map update function
+function issLocation() {
+    fetch('http://api.open-notify.org/iss-now.json').then((res) => {
+        res.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                long.textContent = data.iss_position.longitude
+                lat.textContent = data.iss_position.latitude
+                mymap.setView([data.iss_position.latitude, data.iss_position.longitude], 2)
+                marker.setLatLng([data.iss_position.latitude, data.iss_position.longitude])
+            }
+        })
+    })
+}
+
+// Update map on page load
+window.addEventListener('load', (e) => {
+    issLocation()
+})
+
+// Update map every 10 seconds
+setInterval(() => {
+    issLocation()
+}, 10000)
+
---
+++ src/app.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'ISS Locator',
+        name: 'Peter Wolff'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Peter Wolff'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        name: 'Peter Wolff'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Peter Wolff',
+        errorMessage: 'Page not found'
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
+})
